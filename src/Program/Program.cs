6e912ebcf0495de70491d24329d889c6cd635101
JuanBrignone <juan.brignone@correo.ucu.edu.uy>
2023-04-26 20:28:27 -0300

    Segundo personaje creado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,6 +9,11 @@ namespace RolePLay
             Enano enano1 = new Enano("Gonza", 100);
             Item item1 = new Item("Arma", "Martillo", 15, 0);
             Item item2 = new Item("Armadura", "Pechera", 0, 20); 
+
+            Mago mago1 = new Mago("Juan", 100);
+            Item item3 = new Item("Arma","Espada",10,0);
+            Item item4 = new Item("Arma","Pistola",15,0)
+            
         }
     }
 }
