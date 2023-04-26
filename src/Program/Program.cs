452e6e731069616edc019a87aa9c612310b4922b
Gonzalo-Bergengruen <Gonzalo.Bergengruen@correo.ucu.edu.uy>
2023-04-26 20:21:14 -0300

    1er personaje creado.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
-namespace Program
+namespace RolePLay
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Enano enano1 = new Enano("Gonza", 100);
+            Item item1 = new Item("Arma", "Martillo", 15, 0);
+            Item item2 = new Item("Armadura", "Pechera", 0, 20); 
         }
     }
 }
