cc40dbb2b82d7bcf87fc3433277b56ba90fd85b6
JuanBrignone <juan.brignone@correo.ucu.edu.uy>
2023-05-15 20:51:58 -0300

    Los malos

--- src/Library/LosMalos.cs
+++ src/Library/LosMalos.cs
@@ -2,51 +2,13 @@ using System.Collections.Generic;
 
 namespace RolePLay
 {
-    public class LosMalos : IAtacable
+    public class LosMalos : Personaje, IAtacable
     {
-        string Nombre {get;}
-        int Salud {get; set;}
-        public List<Item> listaItems = new List<Item>();
 
-        public LosMalos (string nombre, int salud)
+        public LosMalos (string nombre, int salud): base(nombre, salud)
         {
-            this.Nombre = nombre;
-            this.Salud = salud;
+            
         }
 
-        public void AgregarItem(Item item)
-        {
-            listaItems.Add(item);
-        }
-
-        public void SacarItem(Item item)
-        {
-            listaItems.Remove(item);
-        }
-
-        public void RecibirAtaque   (int daño)
-        {
-            this.Salud = this.Salud - (this.ObtenerDefensa() - daño);
-        }
-
-        public int ObtenerAtaque()
-        {
-            int AtaqueTotal = 0;
-            for (int i = 0; i < listaItems.Count; i++)
-            {
-                AtaqueTotal += listaItems[i].Daño;
-            }
-            return AtaqueTotal;
-        }
-
-        public int ObtenerDefensa()
-        {
-            int DefensaTotal = 0;
-            for(int i = 0; i < listaItems.Count; i++)
-            {
-                DefensaTotal += listaItems[i].Defensa;
-            }
-            return DefensaTotal;
-        }
     }
 }
