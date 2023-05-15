53cc62f3f57fbcb7a4b620b06129f08a1de15f03
Gonzalo-Bergengruen <Gonzalo.Bergengruen@correo.ucu.edu.uy>
2023-05-15 20:25:53 -0300

    Clases Personaje y Heroe creados, y herencias hechas.

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -2,56 +2,11 @@ using System.Collections.Generic;
 
 namespace RolePLay
 {
-    public class Elfo : IAtacable
+    public class Elfo : Heroe, IAtacable
     {
-        string Nombre {get;}
-        int Salud {get; set;}
-        public List<Item> listaItems = new List<Item>();
-
         public Elfo (string nombre, int salud)
+            :base(nombre, salud)
         {
-            this.Nombre = nombre;
-            this.Salud = salud;
-        }
-
-        public void AgregarItem(Item item)
-        {
-            listaItems.Add(item);
-        }
-
-        public void SacarItem(Item item)
-        {
-            listaItems.Remove(item);
-        }
-
-        public void RecibirAtaque(int daño)
-        {
-            this.Salud = this.Salud - (this.ObtenerDefensa() - daño);
-        }
-
-        public int ObtenerAtaque()
-        {
-            int AtaqueTotal = 0;
-            for (int i = 0; i < listaItems.Count; i++)
-            {
-                AtaqueTotal += listaItems[i].Daño;
-            }
-            return AtaqueTotal;
-        }
-
-        public int ObtenerDefensa()
-        {
-            int DefensaTotal = 0;
-            for(int i = 0; i < listaItems.Count; i++)
-            {
-                DefensaTotal += listaItems[i].Defensa;
-            }
-            return DefensaTotal;
-        }
-
-        public void Curar()
-        {
-            this.Salud = 100;
         }
     }
 }
--- src/Library/Mago.cs
+++ src/Library/Mago.cs
@@ -1,57 +1,11 @@
-using System.Collections.Generic;
-
 namespace RolePLay
 {
-    public class Mago : IAtacable
+    public class Mago : Heroe, IAtacable
     {
-        string Nombre {get;}
-        int Salud {get; set;}
-        public List<Item> listaItems = new List<Item>();
-
+        
         public Mago (string nombre, int salud)
+            :base(nombre, salud)
         {
-            this.Nombre = nombre;
-            this.Salud = salud;
-        }
-
-        public void AgregarItem(Item item)
-        {
-            listaItems.Add(item);
-        }
-
-        public void SacarItem(Item item)
-        {
-            listaItems.Remove(item);
-        }
-
-        public void RecibirAtaque(int daño)
-        {
-            this.Salud = this.Salud - (this.ObtenerDefensa() - daño);
-        }
-
-        public int ObtenerAtaque()
-        {
-            int AtaqueTotal = 0;
-            for (int i = 0; i < listaItems.Count; i++)
-            {
-                AtaqueTotal += listaItems[i].Daño;
-            }
-            return AtaqueTotal;
-        }
-
-        public int ObtenerDefensa()
-        {
-            int DefensaTotal = 0;
-            for(int i = 0; i < listaItems.Count; i++)
-            {
-                DefensaTotal += listaItems[i].Defensa;
-            }
-            return DefensaTotal;
-        }
-
-        public void Curar()
-        {
-            this.Salud = 100;
         }
     }
 }
